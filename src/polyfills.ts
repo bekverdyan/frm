90478f35acad0da154fdf890bd6a54f45bb66dec
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-01-14 00:21:05 +0400

    rearrange imports

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -83,10 +83,10 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-import * as process from 'process';
-window['process'] = process;
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import * as process from 'process';
+window['process'] = process;
