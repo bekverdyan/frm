be6aad7b3faf83bc2ae63d7a7a66e3404fdaf926
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-01-13 20:59:17 +0400

    Fixed routing issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RuleDetailsComponent } from './rules/rule-details/rule-details.component';
+import { RulesManagementComponent } from './rules/rules-management/rules-management.component';
 
-const routes: Routes = [];
+const mainRoutes: Routes = [
+  { path: 'ruleDetails/:id', component: RuleDetailsComponent },
+  { path: 'management', component: RulesManagementComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(mainRoutes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
