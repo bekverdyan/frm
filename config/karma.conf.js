40b3c9684ab746f44642c2a89c823e905d566bfc
arambekverdyan <aram.bekverdyan@gmail.com>
2019-01-31 19:37:54 +0400

    Path to spec-bundle correction

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
      * we are building the test environment in ./spec-bundle.js
      */
     files: [
-      { pattern: './config/spec-bundle.js', watched: false },
+      { pattern: '../config/spec-bundle.js', watched: false },
     ],
     plugins: [
       require('karma-jasmine'),
