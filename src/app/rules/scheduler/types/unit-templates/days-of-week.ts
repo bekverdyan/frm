a2b6493d520f429da6279e6ad113074216a5ddfe
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-02-14 00:29:47 +0400

    Added time unit types

---
+++ src/app/rules/scheduler/types/unit-templates/days-of-week.ts
@@ -0,0 +1,9 @@
+export enum DaysOfWeek {
+    sunday = 'Sunday',
+    monday = 'Monday',
+    tuesday = 'Tuesday',
+    wednesday = 'Wednesday',
+    thursday = 'Thursday',
+    friday = 'Friday',
+    saturday = 'Saturday'
+}
